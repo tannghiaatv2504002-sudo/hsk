e30a97e8c5b228254f13521d7cf3c11ffe848e09
tannghiaatv2504002-sudo <tannghiaatv2504002@gmail.com>
2026-09-05 17:07:04 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,4 @@
+// HSK Go V4.0 - Firebase Web Config
+// Để bật Cloud cho TẤT CẢ người dùng, thay null bằng object config từ Firebase Console.
+// Ví dụ: window.HSK_FIREBASE_CONFIG = { apiKey:'...', authDomain:'...', projectId:'...', appId:'...' };
+window.HSK_FIREBASE_CONFIG = null;
